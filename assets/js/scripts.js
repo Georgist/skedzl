afb0cf125bfe442f53585ce82f143505ae84c9d4
Erik Rehák <rehak.erik@gmail.com>
2018-02-11 19:56:56 +0100

    storing, stringyfying, parsing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,7 @@
 var taskClass = {
-    array: [],
+    dataField: [],
     init: function(){
         this.cacheDOM();
-        this.clearChecked();
         this.addListeners();
     },
     cacheDOM: function(){
@@ -11,24 +10,31 @@ var taskClass = {
         this.label = this.list.querySelectorAll('label');
         this.checkbox = this.list.querySelectorAll('input[type=checkbox]');
     },
-    storeChecked: function() {
-        this.itemAttr = this.getAttribute('data-index');
-        if(_.includes(this.array, this.itemAttr)) {
-            localStorage.setItem('item-index', this.itemAttr);
-        }
-    },
-    clearChecked: function() {
-
+    initChecked: function(){
     },
     addListeners: function(){
         for (var i = 0; i < this.item.length; i++) {
-            taskClass.item[i].addEventListener('click', function(e) {
-                this.classList.toggle('is-selected');
-                e.preventDefault();
-                /* set index to localstorage */
-                this.storeChecked();
-            });
             taskClass.item[i].setAttribute('data-index', [i]);
+            if(_.includes(JSON.parse(localStorage.getItem('checked-items')), taskClass.item[i].getAttribute('data-index'))) {        
+                taskClass.item[i].classList.add('is-selected');
+            }
+            taskClass.item[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                this.itemAttr = this.getAttribute('data-index');
+                this.classList.toggle('is-selected');
+                /* if array does not contain data attribute, add it to array */
+                if(!_.includes(taskClass.dataField, this.itemAttr)) {
+                    taskClass.dataField.push(this.itemAttr);
+                    /* set json to localstorage */
+                    localStorage.setItem('checked-items', JSON.stringify(taskClass.dataField));
+                } else {
+                    /* if array does contain data attribute, remove it from array */
+                    _.pull(taskClass.dataField, this.itemAttr);
+                    /* set json to localstorage */
+                    localStorage.setItem('checked-items', JSON.stringify(taskClass.dataField));
+                }
+                // this.storeChecked();
+            });
         }
     }
 };
