0e1117bed1156fcb1c5e126524a8972370c46d3d
I'm Georgeo <rehak.erik@gmail.com>
2018-02-17 00:58:55 +0100

    links

--- modules/assets.php
+++ modules/assets.php
@@ -1,4 +1,5 @@
 <?php include_once 'analytics.php'; ?>
 <!-- <script src="public/js/scripts.min.js"></script> -->
 <script src="//cdn.jsdelivr.net/npm/lodash@4.17.5/lodash.min.js"></script>
-<script src="assets/js/scripts.js"></script>
+<!-- <script src="assets/js/scripts.js"></script> -->
+<script src="public/js/scripts.min.js"></script>
