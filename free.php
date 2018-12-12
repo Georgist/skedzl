c36d77d182ecd1e9115374a7bd117ae0933b31ab
Erik Rehák <rehak.erik@gmail.com>
2018-12-12 20:23:03 +0100

    update schedule list

--- free.php
+++ free.php
@@ -19,135 +19,137 @@
                     <span class="js-count-current">-</span>&nbsp;&#47;&nbsp;<span class="js-count-total">-</span>
                 </div>
                 <ul class="task-list js-task-list">
-                    <span class="h-main-4"><span>Pred prácou</span></span>
+                    <span class="h-main-4"><span>Ráno</span></span>
+
                     <li>
                         <label for="checkbox-1" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-1">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Menšie upratovanie</span>
+                            <span class="fc-checkbox-text">Upratovanie</span>
                          </label>
                     </li>
                     <li>
                         <label for="checkbox-2" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-2">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Studená sprcha</span>
+                            <span class="fc-checkbox-text">Raňajky</span>
                          </label>
                     </li>
                     <li>
                         <label for="checkbox-3" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-3">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Meditácia</span>
+                            <span class="fc-checkbox-text">Prehľad newslettrov a emailov</span>
                          </label>
                     </li>
                     <li>
                         <label for="checkbox-4" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-4">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Príprava do práce</span>
+                            <span class="fc-checkbox-text">Rozcvička</span>
                          </label>
                     </li>
+
                     <li>
                         <label for="checkbox-5" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-5">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Obchod</span>
+                            <span class="fc-checkbox-text">Meditácia</span>
                          </label>
                     </li>
-
-                    <span class="h-main-4"><span>V práci</span></span>
                     <li>
                         <label for="checkbox-6" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-6">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Raňajky</span>
+                            <span class="fc-checkbox-text">Učenie sa web technológií</span>
                          </label>
                     </li>
+
+                    <span class="h-main-4"><span>Obed</span></span>
                     <li>
                         <label for="checkbox-7" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-7">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Práca</span>
+                            <span class="fc-checkbox-text">Obed</span>
                          </label>
                     </li>
                     <li>
                         <label for="checkbox-8" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-8">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Obed</span>
+                            <span class="fc-checkbox-text">Chill</span>
                          </label>
                     </li>
                     <li>
                         <label for="checkbox-9" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-9">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Práca</span>
+                            <span class="fc-checkbox-text">Učenie sa web technológií</span>
                          </label>
                     </li>
+
                     <li>
                         <label for="checkbox-10" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-10">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Olovrant</span>
-                        </label>
+                            <span class="fc-checkbox-text">Knižka</span>
+                         </label>
                     </li>
-
-                    <span class="h-main-4"><span>Po práci</span></span>
                     <li>
                         <label for="checkbox-11" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-11">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Workout</span>
-                        </label>
+                            <span class="fc-checkbox-text">Gitara</span>
+                         </label>
                     </li>
                     <li>
                         <label for="checkbox-12" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-12">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Sprcha</span>
-                        </label>
+                            <span class="fc-checkbox-text">Workout</span>
+                         </label>
                     </li>
                     <li>
                         <label for="checkbox-13" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-13">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Večera</span>
-                        </label>
-                    </li>
-                    <li>
-                        <label for="checkbox-14" class="fc-checkbox">
-                            <input type="checkbox" id="checkbox-14">
-                            <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Chill</span>
-                        </label>
+                            <span class="fc-checkbox-text">Sprcha</span>
+                         </label>
                     </li>
 
                     <span class="h-main-4"><span>Večer</span></span>
                     <li>
+                        <label for="checkbox-14" class="fc-checkbox">
+                            <input type="checkbox" id="checkbox-14">
+                            <span class="fc-checkbox-icon"></span>
+                            <span class="fc-checkbox-text">Večera</span>
+                         </label>
+                    </li>
+                    <li>
                         <label for="checkbox-15" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-15">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Gitara</span>
-                        </label>
+                            <span class="fc-checkbox-text">Chill</span>
+                         </label>
                     </li>
                     <li>
                         <label for="checkbox-16" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-16">
                             <span class="fc-checkbox-icon"></span>
                             <span class="fc-checkbox-text">Učenie sa web technológií</span>
-                        </label>
+                         </label>
                     </li>
+
                     <li>
                         <label for="checkbox-17" class="fc-checkbox">
                             <input type="checkbox" id="checkbox-17">
                             <span class="fc-checkbox-icon"></span>
-                            <span class="fc-checkbox-text">Chill</span>
+                            <span class="fc-checkbox-text">Film</span>
                          </label>
                     </li>
                     <li>
-                        <label for="checkbox-18" class="fc-checkbox">
-                            <input type="checkbox" id="checkbox-18">
+                        <label for="checkbox-24" class="fc-checkbox">
+                            <input type="checkbox" id="checkbox-24">
                             <span class="fc-checkbox-icon"></span>
                             <span class="fc-checkbox-text">Spánok</span>
                          </label>
