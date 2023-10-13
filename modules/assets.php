25d4d09df6eb250dc0bfe29ee0bb9381bb8eb545
Erik <rehak.erik@gmail.com>
2023-10-13 09:01:31 +0200

    change from php to html format
    change from php to html format

--- modules/assets.php
+++
@@ -1,5 +0,0 @@
-<?php include_once 'analytics.php'; ?>
-<!-- <script src="public/js/scripts.min.js"></script> -->
-<script src="//cdn.jsdelivr.net/npm/lodash@4.17.5/lodash.min.js"></script>
-<!-- <script src="assets/js/scripts.js"></script> -->
-<script src="public/js/scripts.min.js"></script>
